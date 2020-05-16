6817aa5e031c0c88f5d11b6b31b7d444f08150d3
Ishan Muzumdar <imuzumdar@Ishans-MacBook-Pro.local>
2020-05-16 03:01:51 -0400

    updated twitter analysis

--- twitter_analysis/twitter_analysis.R
+++ twitter_analysis/twitter_analysis.R
@@ -2,6 +2,7 @@ library(rtweet)
 library(dplyr)
 library(tokenizers)
 library(tidytext)
+library(syuzhet)
 
 #establishing API connection
 twitter_token <- create_token(
@@ -36,3 +37,29 @@ words <- words[-which(words$word == "coronavirus"),]
 
 wordcloud(words=words$word,freq = words$n, min.freq=3, random.order=FALSE, rot.per=0.35,
           colors=brewer.pal(8, "Dark2"))
+
+
+### SENTIMENT ANALYSIS (sa = sentiment analysis)
+# Converting tweets to ASCII to trackle strange characters
+sa_tweets <- iconv(tweets, from="UTF-8", to="ASCII", sub="")
+# removing retweets, in case needed 
+sa_tweets <-gsub("(RT|via)((?:\\b\\w*@\\w+)+)","",sa_tweets)
+# removing mentions, in case needed
+sa_tweets <-gsub("@\\w+","",sa_tweets)
+covid_sentiment<-get_nrc_sentiment((sa_tweets))
+
+covid_sentimentscores<-data.frame(colSums(covid_sentiment[,]))
+
+names(covid_sentimentscores) <- "Score"
+
+covid_sentimentscores <- cbind("sentiment"=rownames(covid_sentimentscores),
+                               covid_sentimentscores)
+rownames(covid_sentimentscores) <- NULL
+
+# plotting distribution of sentiment
+ggplot(data=covid_sentimentscores,aes(x=sentiment,y=Score))+
+  geom_bar(aes(fill=sentiment),stat = "identity")+
+  theme(legend.position="none")+
+  xlab("Sentiments")+ylab("Scores")+
+  ggtitle("Sentiments Related to COVID-19 Pandemic")+
+  theme_minimal()
